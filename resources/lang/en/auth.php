7b197e7fa896a7f8b71d336e4dd4d2f2233d0ef6
unknown <z.452.k5@gmail.com>
2020-06-21 20:26:21 +0700

    fix_form_vue_text

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Такого емаила не существует в нашей базе данных',
+    'throttle' => 'Слишком много попыток. Пожалуйства, попоробуй через :seconds секунд.',
 
 ];
